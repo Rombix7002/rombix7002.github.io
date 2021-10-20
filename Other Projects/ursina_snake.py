f04f29b77c7f6c96cbcffa8039e0349187dae7b0
Rombix7002 <71732436+Rombix7002@users.noreply.github.com>
2021-10-20 13:19:08 -0300

    Create ursina_snake.py
    [es] Este juego está en desarrollo lo que significa que no está terminado aún.
    Para jugarlo necesitas instalar ursina en https://www.ursinaengine.org
    
    [us] coso

---
+++ Other Projects/ursina_snake.py
@@ -0,0 +1,32 @@
+#in development
+from ursina import *
+from ursina.texture_importer import load_texture
+import time
+import random
+
+def update():
+  snake.y += held_keys['w'] * .1
+  snake.y -= held_keys['s'] * .1
+  snake.x += held_keys['d'] * .1
+  snake.x -= held_keys['a'] * .1
+
+app = Ursina()
+
+snake = Entity(
+  model ='quad', 
+  color = color.green, 
+  scale = (5,1), 
+  position = (-3,0), 
+  collider = 'box'
+  #texture = 'snake textures.png'
+)
+
+manzana = Entity(
+  model ='circle', 
+  color = color.red, 
+  scale = (1,1), 
+  position = (3,0), 
+  collider = 'box'
+)
+app.run()
+
